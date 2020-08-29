afd13d1861bbe7162442af48eb54e1eb8b08bab5
Pesti Péter <pestipeti@gmail.com>
2020-08-29 12:05:38 +0200

    Initial commit

---
+++ src/graphnode.h
@@ -0,0 +1,54 @@
+#ifndef GRAPHNODE_H_
+#define GRAPHNODE_H_
+
+#include <vector>
+#include <string>
+#include <memory>
+#include "chatbot.h"
+
+
+// forward declarations
+class GraphEdge;
+
+class GraphNode
+{
+private:
+
+    // data handlers (owned)
+
+    // Edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges;
+
+    // data handlers (not owned)
+    ChatBot _chatBot;
+    // Edges to the preceding nodes
+    std::vector<GraphEdge *> _parentEdges;
+
+    // proprietary members
+    int _id;
+    std::vector<std::string> _answers;
+
+public:
+    // constructor / destructor
+    GraphNode(int id);
+    ~GraphNode();
+
+    // getter / setter
+    int GetID() { return _id; }
+    int GetNumberOfChildEdges() { return _childEdges.size(); }
+    GraphEdge *GetChildEdgeAtIndex(int index);
+    std::vector<std::string> GetAnswers() { return _answers; }
+    int GetNumberOfParents() { return _parentEdges.size(); }
+
+    // proprietary functions
+    void AddToken(std::string token); // add answers to list
+    void AddEdgeToParentNode(GraphEdge *edge);
+
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
+
+    void MoveChatbotHere(ChatBot chatbot);
+
+    void MoveChatbotToNewNode(GraphNode *newNode);
+};
+
+#endif /* GRAPHNODE_H_ */
